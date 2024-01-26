8ef6e4070ca482e1e09fa6479a92a87df69a3042
Ian Beckett <redacted>
2024-01-25 19:59:40 -0600

    added instructions for neovim setup

--- setup_neovim.sh
+++ setup_neovim.sh
@@ -3,4 +3,5 @@
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# TODO:do PackerSync etc from here?
+# TODO: do PackerSync etc via script?
+echo "now do: evim, :so packer, :PackerSync"
