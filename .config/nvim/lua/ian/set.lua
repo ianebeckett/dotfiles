f0ef0041322fc5fcdc642d6fd68012d0437a6ddb
Ian Beckett <redacted>
2024-02-15 03:31:47 -0600

    <type>(optional scope): <feat>(all) testing commit msg

--- .config/nvim/lua/ian/set.lua
+++ .config/nvim/lua/ian/set.lua
@@ -10,6 +10,10 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+--TODO
+--set splitbelow
+--set splitright
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -25,7 +29,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 9 --all the single-digit relative numbers look like a bracket
+vim.opt.scrolloff = 10 --all the single-digit relative numbers look like a bracket
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = { "80" }
--- setup_after_apt.zsh
+++ setup_after_apt.zsh
@@ -5,3 +5,6 @@ sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
 
 # update tldr db
 tldr --update
+
+# move git-commit-msg
+mv ~/.dotfiles/.git-commit-msg ~
