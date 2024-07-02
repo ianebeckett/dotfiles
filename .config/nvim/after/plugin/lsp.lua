11126a3c5295f189f6b9a9d70914b28b0f6f30c8
Ian Beckett <redacted>
2024-07-02 17:49:47 -0500

    C-y for confirm cmp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,13 +5,13 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp_zero.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     --['<C-y>'] = cmp.mapping.confirm({ select = true }),
     --["<C-Space>"] = cmp.mapping.complete(),
 })
 
 --cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+--cmp_mappings['<S-Tab>'] = nil
 
 cmp.setup({
     window = {
